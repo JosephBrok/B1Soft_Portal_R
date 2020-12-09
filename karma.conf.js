5a436344dbea9e8dbe2675b22e5e30b41fd81ae4
antares233 <ricknapon@hotmail.com>
2020-12-09 11:45:05 -0700

    archivos proyecto inicial agregados

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/portal-reclutamiento'),
+      dir: require('path').join(__dirname, './coverage/portal1'),
       subdir: '.',
       reporters: [
         { type: 'html' },
