a3f019ea7642eda7032125b16db71353092f478c
Ignacio-jipl97 <jose.perezluis@agilethought.com>
2020-12-09 12:52:59 -0600

    changues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+//Aqui va logica para controlar nuestra aplicación
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
---
+++ src/app/pre-registro/pre-registro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-registro',
+  templateUrl: './pre-registro.component.html',
+  styleUrls: ['./pre-registro.component.css']
+})
+export class PreRegistroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
